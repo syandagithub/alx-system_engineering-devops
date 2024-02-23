abf876dce44c02ca63ef9e5ea0bc0c0c5552a8a1
syanda <syandamnt@gmail.com>
2024-02-23 10:56:30 -0800

    ey

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,11 +1,11 @@
 # Installing flask from pip3
 
-package { 'flask':
-  ensure   => '2.1.0',
-  provider => 'pip3',
+package { 'Flask':
+    ensure   => '2.1.0',
+    provider => 'pip3',
 }
 
-package { 'werkzueg':
-  ensure   => '2.1.1',
-  provider => 'pip3',
+package { 'werkzeug':
+    ensure   => '2.1.1',
+    provider => 'pip3',
 }
