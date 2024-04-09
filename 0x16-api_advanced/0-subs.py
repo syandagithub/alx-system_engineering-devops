21c5407358a8f2e2d62bd4537f73d39899d36525
SYANDA MNTAMBO's GITHUB <112751864+syandagithub@users.noreply.github.com>
2024-04-09 14:42:15 +0200

    Create 0-subs.py

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+'''
+    this module contains a function which returns sunscribers
+'''
+import requests
+from sys import argv
+
+
+def number_of_subscribers(subreddit):
+    '''
+        returns the number of subscribers
+    '''
+    user = {'User-Agent': 'Max'}
+    url = requests.get('https://www.reddit.com/r/{}/about.json'
+                       .format(subreddit), headers=user).json()
+    try:
+        return url.get('data').get('subscribers')
+    except Exception:
+        return 0
+
+
+if __name__ == "__main__":
+    number_of_subscribers(argv[1])
