e676ee39fa6e3e103e47baa8377b8d5246bb22bb
SYANDA MNTAMBO's GITHUB <112751864+syandagithub@users.noreply.github.com>
2024-04-09 14:43:51 +0200

    Create 1-top_ten.py

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+'''
+   1-top_ten
+'''
+import requests
+from sys import argv
+
+
+def top_ten(subreddit):
+    '''
+        returns the top ten posts 
+    '''
+    user = {'User-Agent': 'Max'}
+    url = requests.get('https://www.reddit.com/r/{}/hot/.json?limit=10'
+                       .format(subreddit), headers=user).json()
+    try:
+        for post in url.get('data').get('children'):
+            print(post.get('data').get('title'))
+    except Exception:
+        print(None)
+
+
+if __name__ == "__main__":
+    top_ten(argv[1])
